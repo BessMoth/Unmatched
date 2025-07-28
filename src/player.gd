114d85c2df8ed59c5a43e9ce20e5486bf962ed78
BessMoth <consquestsstudio@gmail.com>
2025-07-29 01:21:43 +0200

    add/: left right movement

--- src/player.gd
+++ src/player.gd
@@ -2,24 +2,24 @@ extends Node2D
 
 @export var radius:float = 100.0
 @export var speed:float= 100.0
-@export var turn_center:Vector2 = Vector2(100, 0)
+@export var turn_center:Vector2 = Vector2(200, 0)
 @onready var color_rect: ColorRect = $ColorRect
 @onready var rect_center:Vector2 = color_rect.position + Vector2(color_rect.size.x/2, color_rect.size.y/2);
 
 func _ready() -> void:
 	turn_center += rect_center
+	color_rect.position.x= (turn_center.x - color_rect.size.x/2) - turn_center.x/2
 	
 func _process(delta: float) -> void:
 	pass
 	
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_up"):
-		#color_rect.position.x += turn_center.x/2 - color_rect.size.x
 		pass
 	if event.is_action_pressed("ui_left"):
-		
-		pass
+		color_rect.position.x = (turn_center.x - color_rect.size.x/2) - turn_center.x/2
 	if event.is_action("ui_right"):
+		color_rect.position.x = (turn_center.x - color_rect.size.x/2) + turn_center.x/2
 		pass
 
 func _draw() -> void:
