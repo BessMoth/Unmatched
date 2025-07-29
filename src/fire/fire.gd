01979f6f41212bf3cf61cf234960e0ba8c145bd1
BessMoth <consquestsstudio@gmail.com>
2025-07-29 22:05:38 +0200

    add/: != direction for spwan

--- src/fire/fire.gd
+++ src/fire/fire.gd
@@ -1,7 +1,24 @@
 extends Node2D
 class_name Fire;
 
+enum Directions{ DOWN, LEFT, RIGHT }
+
+var direction: Directions
 @export var speed:float= 100
 
 func _process(delta: float) -> void:
-	self.position.x += speed * delta
+	if direction == Directions.DOWN:
+		goDown(delta)
+	elif direction == Directions.LEFT:
+		goLeft(delta)
+	elif direction == Directions.RIGHT:
+		goRight(delta) 	
+
+func goDown(delta:float)->void:
+	self.position.y += speed * delta
+	
+func goLeft(delta:float)->void:
+	self.position.x -= speed * delta
+	
+func goRight(delta)->void:
+	self.position.x +=speed * delta
--- src/spawn/spawn.gd
+++ src/spawn/spawn.gd
@@ -1,14 +1,21 @@
 extends Node2D
 class_name Spawn
 
+@onready var color_rect: ColorRect = $ColorRect
+
 @export var delay:float = 2
 @export var spawn_scene: PackedScene = load("res://src/fire/fire.tscn")
+@export var spawn_direction: Fire.Directions = Fire.Directions.DOWN
+
+var instance: Fire;
 
 func _process(_delta: float) -> void:
 	spwan()
 	
 func spwan()->void:
-	add_child(spawn_scene.instantiate())
+	instance = spawn_scene.instantiate()
+	instance.direction = spawn_direction
+	add_child(instance)
 	set_process(false)
 	await get_tree().create_timer(delay).timeout
 	set_process(true)
