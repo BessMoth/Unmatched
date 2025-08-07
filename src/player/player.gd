99f3869126b58a69414bad03617a25a889ad2d77
BessMoth <consquestsstudio@gmail.com>
2025-08-07 02:05:13 +0200

    add/: tween

--- src/player/player.gd
+++ src/player/player.gd
@@ -6,26 +6,31 @@ class_name Player
 @export var turn_center:Vector2 = Vector2(300, 0)
 @onready var hand_center:Vector2 = hand.position + Vector2(hand.size.x/2, hand.size.y/2);
 @onready var hand_center_turn:float # set hand in the center 
+var tween: Tween
 
 func _ready() -> void:
+	# for turn
 	turn_center += hand_center
 	hand_center_turn = (turn_center.x - hand.size.x/2)
 	hand.position.x = hand_center_turn - turn_center.x/2
 	
-func _process(delta: float) -> void:
-	pass
-	
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("up"):
-		hand.position.x = hand_center_turn
-		hand.position.y = -hand_center_turn + hand_center_turn
+		move_animation(hand_center_turn, -hand_center_turn + hand_center_turn)
 	if event.is_action_pressed("left"):
-		hand.position.x = hand_center_turn - turn_center.x/2
-		hand.position.y = turn_center.y - hand.size.y/2
+		move_animation(hand_center_turn - turn_center.x/2, turn_center.y - hand.size.y/2)
 	if event.is_action("right"):
-		hand.position.x = hand_center_turn + turn_center.x/2
-		hand.position.y = turn_center.y - hand.size.y/2
+		move_animation(hand_center_turn + turn_center.x/2, turn_center.y - hand.size.y/2)
 
+func new_tween():
+	if tween and tween.is_running():
+		tween.kill()
+	tween = create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
+	
+func move_animation(x:float, y:float):
+	new_tween()
+	tween.tween_property(hand, "position",Vector2(x, y), 2)
+	
 func _draw() -> void:
 	draw_circle(turn_center, 10, Color.ALICE_BLUE)
 
